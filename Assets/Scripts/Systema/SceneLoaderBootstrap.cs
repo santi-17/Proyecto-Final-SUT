ccea34ffbd163a26fe17b8fb391742e15ae42b7e
Santiago Sibilia <98988701+santi-17@users.noreply.github.com>
2025-08-13 16:05:19 -0300

    Pagina de carga arreglada

--- Assets/Scripts/Systema/SceneLoaderBootstrap.cs
+++ Assets/Scripts/Systema/SceneLoaderBootstrap.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
 using System.IO;
 using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoaderBootstrap : MonoBehaviour
 {
     private static SceneLoaderBootstrap _instance;
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public TextMeshProUGUI textoProgreso;
+    private string nombreCampo;
 
     [Header("Prueba en Editor (opcional)")]
     public bool autoLoadOnStart = false;   // marcá en el Inspector si querés probar
@@ -52,7 +61,29 @@ public class SceneLoaderBootstrap : MonoBehaviour
             Debug.LogError($"[SceneLoader] La escena '{sceneName}' no está en Build Settings o el nombre no coincide.");
             return;
         }
+        //ESTO LO HICE YO SANTI
+        //nombreCampo = ObtenerNombreCampo();
 
+        
+
+        if (textoProgreso != null)
+        {
+            if (sceneName == "AradoDisco")
+                textoProgreso.text = $"Cargando la escena de Disquera";
+            else if (sceneName.Equals("Arado"))
+                textoProgreso.text = $"Cargando la escena de Arado";
+            else if (sceneName.Equals("Sembradora"))
+                textoProgreso.text = $"Cargando la escena de Sembrado"; 
+            else if (sceneName.Equals("Riego"))
+                textoProgreso.text = $"Cargando la escena de Riego";
+            else if (sceneName.Equals("Fitosanitario"))
+                textoProgreso.text = $"Cargando la escena de Fitosanitaria";
+            else
+                textoProgreso.text = $"Campo Desconocido";
+            
+        }
+        StartCoroutine(LoadAsynchronously(sceneName));
+        //ACA TERMINA
         Debug.Log($"[SceneLoader] Cargando escena: {sceneName}");
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
     }
@@ -68,8 +99,38 @@ public class SceneLoaderBootstrap : MonoBehaviour
         }
         return false;
     }
+    //ESTO LO HICE YO SANTI
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName/*, LoadSceneMode.Single*/);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = (progress * 100f).ToString("F2") + "%";
+            yield return null;
+        }
+    }
 
+    string ObtenerNombreCampo()
+    {
+        string sceneName = SceneManager.GetActiveScene().name.ToLower();
 
+        if (sceneName.Contains("aradodisco"))
+            return "Disquera";
+        else if (sceneName.Contains("arado"))
+            return "Arado";
+        else if (sceneName.Contains("sembradora"))
+            return "Sembradora";
+        else if (sceneName.Contains("riego"))
+            return "Regadora";
+        else if (sceneName.Contains("fitosanitario"))
+            return "Fitosanitario";
+        else
+            return "Campo Desconocido";
+    }
+    //ACA TERMINA
     public class SceneLogger : MonoBehaviour
     {
         void OnEnable() { SceneManager.sceneLoaded += OnSceneLoaded; }
